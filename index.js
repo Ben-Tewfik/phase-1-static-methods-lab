7194a844425516976c00aebe6c7c377502333d59
Benarba Tewfik <tewfikhotspur@gmail.com>
2023-09-05 21:07:32 +0200

    Completed assignment

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
 class Formatter {
   //add static methods here
+  static capitalize(word) {
+    const firstLetterCap = word.charAt(0).toUpperCase() + word.slice(1);
+    return firstLetterCap;
+  }
+  static sanitize(word) {
+    const newWord = word.replace(/[^A-Za-z0-9-' ]+/g, "");
+    return newWord;
+  }
+
+  static titleize(string) {
+    const excludedWords = [
+      "the",
+      "a",
+      "an",
+      "but",
+      "of",
+      "and",
+      "for",
+      "at",
+      "by",
+      "from",
+    ];
+    const words = string.split(" ");
+    const titleizedWords = words.map((word, index) => {
+      if (index === 0 || !excludedWords.includes(word.toLowerCase())) {
+        return word.charAt(0).toUpperCase() + word.slice(1);
+      } else {
+        return word;
+      }
+    });
+    return titleizedWords.join(" ");
+  }
 }
